0fe97ef55231ee6c6189cda5116a016b2606880d
Lunacy Rising <rerirani20@gmail.com>
2020-08-09 03:48:35 -0300

    refactoring del nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,27 +2,19 @@ import React, { useState } from "react"
 import { useTranslation } from "react-i18next"; 
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { AppBar, Avatar, IconButton, Box, Tooltip, Zoom } from "@material-ui/core";
-import SpeedDial from '@material-ui/lab/SpeedDial';
-import SpeedDialIcon from '@material-ui/lab/SpeedDialIcon';
-import SpeedDialAction from '@material-ui/lab/SpeedDialAction';
 import { makeStyles } from "@material-ui/core/styles";
-import CloseRoundedIcon from '@material-ui/icons/CloseRounded';
+import { AppBar, Avatar, Box} from "@material-ui/core";
 import { changeLanguage } from "../actions/changeLanguage";
-import EnglishBtn from "./buttons/EnglishBtn";
-import SpanishBtn from "./buttons/SpanishBtn";
-import JapaneseBtn from "./buttons/JapaneseBtn";
+import EnglishBtn from "./buttons/langBtns/EnglishBtn";
+import SpanishBtn from "./buttons/langBtns/SpanishBtn";
+import JapaneseBtn from "./buttons/langBtns/JapaneseBtn";
 import pokemon from "../../src/utils/images/pokemon.svg";
-import englishicon from "../../src/utils/images/englishicon.svg";
-import spanishicon from "../../src/utils/images/spanishicon.svg";
-import japaneseicon from "../../src/utils/images/japaneseicon.svg";
 import ThemeToggle from "./buttons/ThemeToggle";
+import Dial from "./buttons/dial/Dial";
 
 const NavBar = () => {
     
-    const [open, setOpen] = useState(false);
-
-    const [openDial, setOpenDial] = useState(false);
+    const [ open, setOpen ] = useState(false);
 
     const dispatch = useDispatch()
     
@@ -33,131 +25,44 @@ const NavBar = () => {
             display: "flex",
             justifyContent: "space-around",
             alignItems: "center",
-            height: "8vh",
-            height: "8vh",
-            "@media(min-width: 320px) and (max-width: 480px)" : {
-                justifyContent: "space-between"
-            }
+            flexDirection: "row",
+            height: "10vh",
         },
         logo: {
-             width: 50,
-             height: 50,
-             "@media(min-width: 320px) and (max-width: 480px)": {
-                display: "none"
-            },
-             "@media(min-width: 320px) and (max-width: 768px)": {
-                width: 40,
-                height: 40,
-            },
+             width: 40,
+             height: 40,
          },
         langBtnsContainer:{
-            "@media(min-width: 320px) and (max-width: 480px)": {
-                display: "none"
-            },
-         },
-        english: {
-            backgroundImage: `url(${englishicon})`,
-            width: 35,
-            height: 35,
-            marginRight: 35
-         },
-        spanish: {
-            backgroundImage: `url(${spanishicon})`,
-            width: 35,
-            height: 35,
-           marginRight: 35
-         },
-        japanese: {
-            backgroundImage: `url(${japaneseicon})`,
-            width: 35,
-            height: 35,
-         },
-        darkModeBtnContainer: {
-            "@media(max-width: 320px)": {
-                marginLeft: 12
-             },
-         },
-        hamburgerBtn: {
-            display:"none",
-            "@media(min-width: 320px) and (max-width: 480px)": {
+            display: "none",
+            "@media(min-width: 768px)": {
                 display: "flex",
-                position: "absolute",
-                right: 26,
+                justifyContent: "space-between",
+                width: "20%"
             },
-            "@media(min-width: 391px) and (max-width: 480px)": {
-                display: "flex"
-            }
-        },
-        spanTooltip: {
-            fontSize: "12px"
-        }, 
-       }));
-       const classes = useStyles();
-       
-       const { logo, container, langBtnsContainer, english, spanish, japanese, darkModeBtnContainer, hamburgerBtn, spanTooltip } = classes;
+         },
+    }));
 
-       const { i18n } = useTranslation();
+    const classes = useStyles();
 
-       const handleClick = (language, number) => {
-            i18n.changeLanguage(language);
-            dispatch(changeLanguage(number));
-       }
+    const {container, logo, langBtnsContainer} = classes;
 
-       const handleOpen = () => {
-        setOpenDial(true);
-      };
-    
-      const handleClose = () => {
-        setOpenDial(false);
-      };
+    const { i18n } = useTranslation();
 
-      const englishTooltip = <span className={spanTooltip}>Translate to english</span>
-      const spanishTooltip = <span className={spanTooltip}>Traducir al español</span>
-      const japaneseTooltip = <span className={spanTooltip}>日本語に翻訳</span>
-
-      const actions = [
-        { icon: <EnglishBtn handleClick={handleClick}/>},
-        { icon: <SpanishBtn handleClick={handleClick}/>},
-        { icon: <JapaneseBtn handleClick={handleClick}/>},
-      ];
+    const handleClick = (language, number) => {
+        i18n.changeLanguage(language);
+        dispatch(changeLanguage(number));
+    }
 
     return(
-        <AppBar position="static"> 
-            <Box className={container}>
-                    <Avatar  component={Link} to={"/"}className={logo} src={pokemon}/>
-                <Box className={langBtnsContainer}>
-                    <Tooltip TransitionComponent={Zoom} title={englishTooltip}>
-                        <IconButton className={english} onClick={() => handleClick("en", 2 )}/>
-                    </Tooltip>
-                    <Tooltip TransitionComponent={Zoom} title={spanishTooltip}>
-                        <IconButton className={spanish} onClick={() => handleClick("es", 4 )}/>
-                    </Tooltip>
-                    <Tooltip TransitionComponent={Zoom} title={japaneseTooltip}>
-                        <IconButton className={japanese} onClick={() => handleClick("ja", 1)}/>
-                    </Tooltip>
-                </Box>
-                {<Box className={darkModeBtnContainer}><ThemeToggle setOpen={setOpen}/></Box>}
-                <Box className={hamburgerBtn}>
-                    <SpeedDial
-                    FabProps={{ size: "small"}}
-                    ariaLabel="SpeedDial openIcon example"
-                    icon={<SpeedDialIcon openIcon={<CloseRoundedIcon />} />}
-                    onClose={handleClose}
-                    onOpen={handleOpen}
-                    open={openDial}
-                    direction={"left"}
-                    >  
-                    {actions.map((action, index) => (
-                        <SpeedDialAction
-                            key={index + 1}
-                            icon={action.icon}
-                            tooltipTitle={""}
-                            onClick={handleClose}
-                        />
-                    ))}
-                    </SpeedDial>
-                </Box>
+        <AppBar className={container} position="fixed"> 
+            <Avatar  component={Link} to={"/"}className={logo} src={pokemon}/>
+            <Box className={langBtnsContainer}>
+                <EnglishBtn handleClick={handleClick}/>
+                <SpanishBtn handleClick={handleClick}/>
+                <JapaneseBtn handleClick={handleClick}/>
             </Box>
+            <ThemeToggle setOpen={setOpen}/>
+            <Dial handleClick={handleClick}/>
         </AppBar>
     )
 }
