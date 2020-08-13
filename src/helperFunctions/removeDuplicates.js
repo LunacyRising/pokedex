d821f879a39efe218e5ddd20aeba4fe251435241
Lunacy Rising <rerirani20@gmail.com>
2020-08-13 17:15:38 -0300

    agregada funcion removeDuplicates2

--- src/helperFunctions/removeDuplicates.js
+++ src/helperFunctions/removeDuplicates.js
@@ -4,3 +4,9 @@ export const removeDuplicates = (arr, key) => {
     let set = new Set();
     return arr.filter(obj => !set.has(obj.data[key]) && set.add(obj.data[key]));
 }
+
+//temporal :3
+export const removeDuplicates2 = (arr, key) => {
+    let set = new Set();
+    return arr.filter(obj => !set.has(obj[key]) && set.add(obj[key]));
+}
