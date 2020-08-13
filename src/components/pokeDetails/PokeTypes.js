efe54ac9bea267604e0d43e6fd307d108fccc887
Lunacy Rising <rerirani20@gmail.com>
2020-08-13 03:10:30 -0300

    cambio en la forma de mostrar los tipos de pokemon

--- src/components/pokeDetails/PokeTypes.js
+++ src/components/pokeDetails/PokeTypes.js
@@ -1,20 +1,35 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { makeStyles } from "@material-ui/core/styles";
-import { Typography, Box } from "@material-ui/core";
-
+import { Box, IconButton } from "@material-ui/core";
+//icons
+import orbz_fire from "../../utils/icons/orbz_fire.ico";
+import orbz_water from "../../utils/icons/orbz_water.ico";
+import orbz_nature from "../../utils/icons/orbz_nature.ico";
+import orbz_spirit from "../../utils/icons/orbz_spirit.ico";
+import orbz_air from "../../utils/icons/orbz_air.ico";
+import orbz_earth from "../../utils/icons/orbz_earth.ico";
+import orbz_lightning from "../../utils/icons/orbz_lightning.ico";
+import orbz_ice from "../../utils/icons/orbz_ice.ico";
+import orbz_machine from "../../utils/icons/orbz_machine.ico";
+import orbz_moon from "../../utils/icons/orbz_moon.ico";
+import orbz_death from "../../utils/icons/orbz_death.ico";
 
   const PokeTypes = ({ types }) => {
 
-    const {t} = useTranslation(); 
-
     const useStyles = makeStyles(() => ({
        
-        element: {
-        "@media(max-width: 320px) and (max-height: 480px)": {
-            marginTop: 0,
-            fontSize: 16
+        btnsContainer: {
+            display: "flex"
         },
+        btn: {
+            width: 48,
+            height: 48,
+            transition: "0.3s ease-in-out",
+            border: "1px solid transparent",
+            "&:hover": {
+                border: `1px solid currentColor`
+            }
         },
         fire: {
             color: "#ff1b00"
@@ -43,62 +58,90 @@ import { Typography, Box } from "@material-ui/core";
         ground: {
             color: "#A86D32"
         },
+        steel: {
+            color: "#aaa9ad"
+        },
+        psychic: {
+            color: "#FF1493"
+        },
         noType: {
             color: "#959996"
         }
       }));
       
-      const classes = useStyles();
-      const { element, fire, water, flying, insect, electric, poison, grass, ground, bug, noType } = classes;
+    const classes = useStyles();
+
+    const { btnsContainer, btn, fire, water, flying, insect, electric, poison, grass, ground, bug, steel, psychic, noType } = classes;
+
+    const { t } = useTranslation();
 
     return (
-        <>  
-            <Box className={element}>
+        <>    
+            <Box className={btnsContainer}>
                 {types.map((type,index) => {
+                    let icon;
                     let color;
                     let word;
                     switch (type.type.name) {
                         case "fire":
+                            icon = orbz_fire; 
                             color = fire;
-                            word = t("Fire")
                             break;
                         case "water":
+                            icon = orbz_water;
                             color = water;
-                            word = t("Water")
+                            break;
+                        case "ice":
+                            icon = orbz_ice; 
+                            color = water;
                             break;
                         case "insect":
+                            icon = orbz_fire; 
                             color = insect;
-                            word = t("Insect")
                             break;
                         case "electric":
+                            icon = orbz_lightning; 
                             color = electric;
-                            word = t("Electric")
                             break;
                         case "poison":
+                            icon = orbz_spirit; 
                             color = poison;
-                            word = t("Poison")
                             break;
                         case "grass":
+                            icon = orbz_nature; 
                             color = grass;
-                            word = t("Grass")
                             break;
                         case "ground":
+                            icon = orbz_earth; 
                             color = ground;
-                            word = t("Ground")
                             break;
                         case "bug":
+                            icon = orbz_death; 
                             color = bug;
-                            word = t("Bug")
                             break;
                         case "flying":
+                            icon = orbz_air; 
                             color = flying;
-                            word = t("Flying")
                             break;
+                        case "air":
+                            icon = orbz_air; 
+                            color = flying;
+                            break;
+                        case "steel":
+                            icon = orbz_machine;
+                            color = steel;
+                            break;
+                        case "psychic":
+                            icon = orbz_moon;
+                            color = psychic; 
+                            break;   
                         default:
                         color = noType;
                         word = t("Unknown")
                     }
-                    return <Typography key={index} className={color} variant="body1">{`${t("Type")}: ${word}`}</Typography>
+                return <IconButton key={index} className={`${btn} ${color}`}>
+                            <img src={icon} width={48} height={48}></img>
+                        </IconButton>    
                 })}
             </Box>
         </>
