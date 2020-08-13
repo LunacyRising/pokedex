93cc3407275ccf22af8c554044e9fbf83bfd0f87
Lunacy Rising <rerirani20@gmail.com>
2020-08-12 23:29:39 -0300

    agregado de traduccion para nombre de habilidad

--- src/components/pokeDetails/PokeAbilities.js
+++ src/components/pokeDetails/PokeAbilities.js
@@ -4,23 +4,35 @@ import { useSelector, useDispatch } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography, Tooltip, Box } from "@material-ui/core";
 import Zoom from '@material-ui/core/Zoom';
-import { getPokemonsAbilitiesDescription } from "../../actions/fetchPokemons";
+//import { getPokemonsAbilitiesDescription } from "../../actions/fetchPokemons";
 import dots from "../../utils/images/dots.svg";
+import { matchingAbilitites } from "../../helperFunctions/matchingAbilitites";
 
-const PokeAbilities = ({ abilities, abilitiesDescriptions, abilityDescriptionLoading }) => {
+const PokeAbilities = ({ abilitiesNames, abilitiesDescriptions, abilityDescriptionLoading }) => {
 
-    const {language} = useSelector(state => state.languageReducer);
+    const { language } = useSelector(state => state.languageReducer);
+
+    const { translatedAbilities } = useSelector(state => state.pokemonsReducer);
 
     const dispatch = useDispatch()
 
     const {t} = useTranslation(); 
 
-    const getAbilityDescription = (abilityName) => {
+    /*const getAbilityDescription = (abilityName) => {
         // habilidad existe
         const abilityExists = abilitiesDescriptions.some(ability => ability.name === abilityName);
         if(!abilityExists) dispatch(getPokemonsAbilitiesDescription({abilityName}));
-    }
-    
+    }*/
+
+    const matchAb = matchingAbilitites(translatedAbilities, abilitiesNames);
+
+    const abilities = [];
+    matchAb.map(item =>{
+        item.names.filter(item => {
+            item.language.name === language &&  abilities.push(item) 
+        })
+    })
+
     const useStyles = makeStyles(() => ({
 
         abilitiesContainer: {
@@ -47,13 +59,20 @@ const PokeAbilities = ({ abilities, abilitiesDescriptions, abilityDescriptionLoa
             fontSize: "12px"
         }, 
       }));
-      const classes = useStyles();
-      const { abilitiesContainer, singleAbilityContainer, singleAbility, spanTooltip } = classes;
+    const classes = useStyles();
+
+    const { abilitiesContainer, singleAbilityContainer, singleAbility, spanTooltip } = classes;
 
     return (
         <>  
             <Box className={abilitiesContainer}>
-                {abilities.map((ability,index)=> {
+                {abilities.map(ability =>(
+                    <Box key={ability.name}className={singleAbilityContainer}>
+                        <span>{t("Ability")}:</span>
+                        <Typography className={singleAbility} variant="subtitle2">{ability.name}</Typography>
+                    </Box>
+                ))}
+                {/*abilities.map((ability,index)=> {
                     
                     let abilityName = ability.ability.name;
                     let skill = abilitiesDescriptions.length > 0 && abilitiesDescriptions.filter(ability => {
@@ -64,13 +83,13 @@ const PokeAbilities = ({ abilities, abilitiesDescriptions, abilityDescriptionLoa
                     const skillSpan = <span className={spanTooltip}>{finalSkill}</span>;
                     const noSkillSpan = <span className={spanTooltip}>{t("Click to see the ability description")}</span>;
 
-                    return  <Tooltip key={index} TransitionComponent={Zoom} title={ abilityDescriptionLoading  ?  <img src={dots} alt="loading"></img> : !finalSkill ? noSkillSpan : skillSpan}>
+                    {/*return  <Tooltip key={index} TransitionComponent={Zoom} title={ abilityDescriptionLoading  ?  <img src={dots} alt="loading"></img> : !finalSkill ? noSkillSpan : skillSpan}>
                                 <Box className={singleAbilityContainer}>
                                     <span>{t("Ability")}:</span>
                                     <Typography className={singleAbility} onClick={() => getAbilityDescription(abilityName)} variant="subtitle2">{abilityName}</Typography>
                                 </Box>
-                            </Tooltip>
-                })}
+                </Tooltip>}
+                })*/}
             </Box>   
 
         </>
